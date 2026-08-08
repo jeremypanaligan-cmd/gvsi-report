2c699a1e4aed261b83ed725978a25fd1d82a953b
jeremypanaligan-cmd <jeremy.panaligan@gllopvision.com>
2026-08-08 23:53:49 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,7 @@
-const CACHE_NAME = 'report-app-v1';
-const urlsToCache = [
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+self.addEventListener('fetch', (e) => {
+  // Pass-through fetch
 });
